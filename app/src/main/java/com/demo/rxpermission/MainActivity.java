4d7a6826d39290cc40814abdc2eae86b0e9137b5
田楠楠 <tiannannan@nutfin.cn>
2017-10-17 15:05:12 +0800

    init

---
+++ app/src/main/java/com/demo/rxpermission/MainActivity.java
@@ -0,0 +1,57 @@
+package com.demo.rxpermission;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.demo.rxpermission_lib.RxPermissions;
+
+import rx.functions.Action1;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tvTel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                requestPression();
+                callTel("10086");
+            }
+        });
+
+    }
+
+    void requestPression(){
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.setLogging(true);
+
+        rxPermissions.request(Manifest.permission.CALL_PHONE).subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean granted) {
+                if(granted){
+                    callTel("10086");
+                }else{
+                    Toast.makeText(MainActivity.this, "缺少权限，无法操作", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    void callTel(String callNum) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + callNum));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }else{
+            Toast.makeText(MainActivity.this, "该设备暂不支持拨打电话", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
