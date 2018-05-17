523b79a2eec43445416c63fb5455e014a4440cd6
tiannannan <tiannannan@nutfin.cn>
2018-05-17 17:27:56 +0800

    rxjava1升级为rxjava2

--- README.md
+++ README.md
@@ -2,23 +2,23 @@
 
 1、在project级build.gradle中添加： maven { url 'https://jitpack.io' }  
 
-2、在app级build.gradle中添加：compile 'com.github.nanyuweiyi:rxpermission:1.2' 
+2、在app级build.gradle中添加：compile 'com.github.nanyuweiyi:rxpermission:1.3' 
 
 3、使用demo:
 
-void requestPression(){
+```
+    void requestPression(){
         RxPermissions rxPermissions = new RxPermissions(this);
-        rxPermissions.setLogging(true);
-
-        rxPermissions.request(Manifest.permission.READ_EXTERNAL_STORAGE).subscribe(new Action1<Boolean>() {
+        rxPermissions.request(Manifest.permission.CALL_PHONE).subscribe(new Consumer<Boolean>() {
             @Override
-            public void call(Boolean granted) {
+            public void accept(Boolean granted) throws Exception {
                 if(granted){
-                    Intent intent = new Intent(mContext, ChoosePicActivity.class);
-                    startActivity(intent);
+                      Toast.makeText(MainActivity.this, "权限OK", Toast.LENGTH_SHORT).show();
+                      callTel("10086");
                 }else{
-                    Toast.makeText(TestActivity.this, "缺少权限，无法操作", Toast.LENGTH_SHORT).show();
+                       Toast.makeText(MainActivity.this, "缺少权限", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+ ```
--- app/src/main/java/com/demo/rxpermission/MainActivity.java
+++ app/src/main/java/com/demo/rxpermission/MainActivity.java
@@ -8,9 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.nanyuweiyi.rxpermission.Permission;
 import com.nanyuweiyi.rxpermission.RxPermissions;
 
-import rx.functions.Action1;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,18 +31,42 @@ public class MainActivity extends AppCompatActivity {
 
     void requestPression(){
         RxPermissions rxPermissions = new RxPermissions(this);
-        rxPermissions.setLogging(true);
 
-        rxPermissions.request(Manifest.permission.CALL_PHONE).subscribe(new Action1<Boolean>() {
+        /*rxPermissions.request(Manifest.permission.CALL_PHONE).subscribe(new Consumer<Boolean>() {
             @Override
-            public void call(Boolean granted) {
+            public void accept(Boolean granted) throws Exception {
                 if(granted){
+                    Toast.makeText(MainActivity.this, "权限OK", Toast.LENGTH_SHORT).show();
                     callTel("10086");
                 }else{
-                    Toast.makeText(MainActivity.this, "缺少权限，无法操作", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "缺少权限", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+
+        rxPermissions.requestEach(Manifest.permission.CALL_PHONE).subscribe(new Consumer<Permission>() {
+            @Override
+            public void accept(Permission permission) throws Exception {
+                if (permission.granted) {
+                    Toast.makeText(MainActivity.this, "权限OK", Toast.LENGTH_SHORT).show();
+                    callTel("10086");
+                } else if (permission.shouldShowRequestPermissionRationale) {
+                    // Denied permission without ask never again
+                    Toast.makeText(MainActivity.this, "权限禁止，会继续询问", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Denied permission with ask never again
+                    // Need to go to the settings
+                    Toast.makeText(MainActivity.this, "权限禁止，不会继续询问", Toast.LENGTH_SHORT).show();
+                            /*//跳转系统设置界面 （有些手机不能跳到该设置界面）
+                            Intent intent = new Intent();
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+                            intent.setData(Uri.fromParts("package", getPackageName(), null));
+                            startActivity(intent);*/
                 }
             }
         });
+
     }
 
     void callTel(String callNum) {
